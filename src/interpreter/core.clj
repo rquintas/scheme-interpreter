d03dde2120f06395a60ac5bed1d155394e558371
rquintas <rquintas@rquintas.net>
2013-09-18 00:04:35 +0100

    Initial commit.

---
+++ src/interpreter/core.clj
@@ -0,0 +1,58 @@
+(ns interpreter.core)
+
+(defn tagged-list? [exp tag]
+    (if (list? exp)
+        (= (first exp) tag) 
+        false))
+
+(defn self-evaluating? [exp]
+    (cond (number? exp) true
+          (string? exp) true
+          :else false))
+
+(defn variable? [exp]
+    (symbol? exp))
+    
+(defn quoted? [exp]
+    (tagged-list? exp 'quote))
+
+(defn assignment? [exp]
+    (tagged-list? exp 'set!))
+    
+(defn definition? [exp]
+    (tagged-list? exp 'define))
+    
+(defn lambda? [exp]
+    (tagged-list? exp 'lambda))
+
+(defn if? [exp]
+    (tagged-list? exp 'if))
+
+(defn begin? [exp]
+    (tagged-list? exp 'begin))
+
+(defn cond? [exp]
+    (tagged-list? exp 'cond))
+
+(defn eval [exp env]
+    (cond (self-evaluating? exp) exp
+          (variable? exp) nil
+          (quoted? exp) nil
+          (assignment? exp) nil
+          (definition? exp) nil
+          (if? exp) nil
+          (lambda? exp) nil
+          (begin? exp) nil
+          (cond? exp) nil
+          ;(application? exp) nil
+          :else nil))
+          
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
+
+(defn -main [] 
+    (foo "queijo")
+    (self-evaluating? :a)
+    (quoted? (list 'quote "Queijo")))
---
+++ test/interpreter/core_test.clj
@@ -0,0 +1,7 @@
+(ns interpreter.core-test
+  (:use clojure.test
+        interpreter.core))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
